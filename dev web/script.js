cb4080ed2802eb6177044c33b76daef97dfad00b
Anusreehh <133986226+Anusreehh@users.noreply.github.com>
2023-05-18 20:30:26 +0530

    Add files via upload

---
+++ dev web/script.js
@@ -0,0 +1,13 @@
+
+function validation(){
+    if(document.getElementById("name-input").value=="")
+    {
+        document.getElementById("name-validation").style.display="block"
+    }
+    if(document.getElementById("email-input").value=="")
+    {
+        document.getElementById("email-validation").style.display="block"
+    }
+    return false;
+
+}
